6eb414e86c9e74c97dcbc7215bbfab0792b87dc6
Stefan Nikolovski <stefannikolovski996@gmail.com>
2022-03-03 08:38:41 -0800

    Update Json-ToCassandra.scala

--- Json-ToCassandra.scala
+++ Json-ToCassandra.scala
@@ -1,4 +1,12 @@
 import spark.implicits._
+import org.apache.spark.sql.SparkSession
+import com.databricks.spark.xml._
+
+import org.apache.spark.sql.functions._
+import com.datastax.spark.connector._
+import org.apache.spark.sql.cassandra._
+import com.datastax.spark.connector.cql._
+
 
 val path = "/home/stefan/Spark/jsonexamples/Data-Person.json"
 val peopleDF = spark.read.json(path)
